8613077c5ebc446ecebd2fddfa657707838ae80f
leumas244 <FreddiW.15>
2024-06-13 11:46:49 +0200

    first test-data and first data class Package

---
+++ main.py
@@ -0,0 +1,16 @@
+class Package:
+    def __init__(self) -> None:
+        self.recipientName: str = None
+        self.address1: str = None
+        self.address2: str = None
+        self.address3: str = None
+        self.country: str = None
+        self.postalCode: int = None
+        self.city: str = None
+        self.state: str = None
+        self.phoneNumber: str = None
+        self.email: str = None
+        self.weight: float = None
+        self.service: str = None
+        self.referenceNumber: str = None
+        self.packageCount: str = None
